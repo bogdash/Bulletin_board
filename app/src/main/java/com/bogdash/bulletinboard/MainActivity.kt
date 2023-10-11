f3720b16399e0a891770e23431dc148fc4e40c9a
Anastasia <ms.anastasia.bogdanova@gmail.com>
2023-10-11 22:07:01 +0300

    add slide-out menu

--- app/src/main/java/com/bogdash/bulletinboard/MainActivity.kt
+++ app/src/main/java/com/bogdash/bulletinboard/MainActivity.kt
@@ -2,10 +2,62 @@ package com.bogdash.bulletinboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.bogdash.bulletinboard.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 
-class MainActivity : AppCompatActivity() {
+private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        var toggle = ActionBarDrawerToggle(this, binding.drawerLayout, findViewById(R.id.toolbar), R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        binding.navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.my_ads -> {
+                Toast.makeText(this, "Pressed my ads", Toast.LENGTH_LONG).show()
+            }
+            R.id.car -> {
+                Toast.makeText(this, "Pressed car", Toast.LENGTH_LONG).show()
+            }
+            R.id.pc -> {
+                Toast.makeText(this, "Pressed pc", Toast.LENGTH_LONG).show()
+            }
+            R.id.smartphone -> {
+                Toast.makeText(this, "Pressed smartphone", Toast.LENGTH_LONG).show()
+            }
+            R.id.household_appliances -> {
+                Toast.makeText(this, "Pressed household_appliances", Toast.LENGTH_LONG).show()
+            }
+            R.id.sign_in -> {
+                Toast.makeText(this, "Pressed sign in", Toast.LENGTH_LONG).show()
+
+            }
+            R.id.sign_up -> {
+                Toast.makeText(this, "Pressed sign up", Toast.LENGTH_LONG).show()
+
+            }
+            R.id.sign_out -> {
+                Toast.makeText(this, "Pressed sign out", Toast.LENGTH_LONG).show()
+
+            }
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
