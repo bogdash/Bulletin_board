630410f631f8852f30f0ac3f4bbdd7d42ce7fd37
Anastasia <ms.anastasia.bogdanova@gmail.com>
2023-10-20 16:42:26 +0300

    update edit activity

--- app/src/main/java/com/bogdash/bulletinboard/activity/EditsAdsActivity.kt
+++ app/src/main/java/com/bogdash/bulletinboard/activity/EditsAdsActivity.kt
@@ -2,8 +2,10 @@ package com.bogdash.bulletinboard.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.bogdash.bulletinboard.R
 import com.bogdash.bulletinboard.databinding.ActivityEditsAdsBinding
+import com.bogdash.bulletinboard.utils.CityHelper
 
 class EditsAdsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditsAdsBinding
@@ -11,5 +13,13 @@ class EditsAdsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditsAdsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            CityHelper.getAllCountries(this)
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spCountry.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/bogdash/bulletinboard/utils/CityHelper.kt
@@ -0,0 +1,32 @@
+package com.bogdash.bulletinboard.utils
+
+import android.content.Context
+import org.json.JSONObject
+import java.io.IOException
+import java.io.InputStream
+
+object CityHelper {
+    fun getAllCountries(context: Context): ArrayList<String> {
+        var tempArray = ArrayList<String>()
+        try {
+            val inputStream: InputStream = context.assets.open("countriesToCities.json")
+            val size: Int = inputStream.available()
+            val bytesArray = ByteArray(size)
+            inputStream.read(bytesArray)
+            val jsonFile = String(bytesArray)
+            val jsonObject = JSONObject(jsonFile)
+            val countriesNames = jsonObject.names()
+
+            if (countriesNames != null) {
+                for (item in 0 until countriesNames.length()) {
+                    tempArray.add(countriesNames.getString(item))
+                }
+            }
+
+        } catch (e: IOException) {
+
+        }
+
+        return tempArray
+    }
+}
