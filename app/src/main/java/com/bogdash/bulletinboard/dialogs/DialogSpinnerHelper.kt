6e00b05beea02863baa367775877811d93179e98
Anastasia <ms.anastasia.bogdanova@gmail.com>
2023-11-02 21:16:07 +0300

    update dialog countries

--- app/src/main/java/com/bogdash/bulletinboard/dialogs/DialogSpinnerHelper.kt
+++ app/src/main/java/com/bogdash/bulletinboard/dialogs/DialogSpinnerHelper.kt
@@ -3,6 +3,8 @@ package com.bogdash.bulletinboard.dialogs
 import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bogdash.bulletinboard.R
 
 class DialogSpinnerHelper {
@@ -10,7 +12,12 @@ class DialogSpinnerHelper {
     fun showSpinnerDialog(context: Context, list: ArrayList<String>) {
         val builder = AlertDialog.Builder(context)
         val rootView = LayoutInflater.from(context).inflate(R.layout.spinner_layout, null)
+        val adapter = RcViewDialogSpinner()
+        val rcView = rootView.findViewById<RecyclerView>(R.id.rcSpView)
+        rcView.layoutManager = LinearLayoutManager(context)
+        rcView.adapter = adapter
         builder.setView(rootView)
+        adapter.updateAdapter(list)
         builder.show()
     }
 }
---
+++ app/src/main/java/com/bogdash/bulletinboard/dialogs/RcViewDialogSpinner.kt
@@ -0,0 +1,38 @@
+package com.bogdash.bulletinboard.dialogs
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bogdash.bulletinboard.R
+
+class RcViewDialogSpinner : RecyclerView.Adapter<RcViewDialogSpinner.SpViewHolder>() {
+    private val mainList = ArrayList<String>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SpViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.sp_list_item, parent, false)
+        return SpViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return mainList.size
+    }
+
+    override fun onBindViewHolder(holder: SpViewHolder, position: Int) {
+        holder.setData(mainList[position])
+    }
+
+    class SpViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun setData(text: String) {
+            val tvSpItem = itemView.findViewById<TextView>(R.id.tvSpItem)
+            tvSpItem.text = text
+        }
+    }
+
+    fun updateAdapter(list: ArrayList<String>) {
+        mainList.clear()
+        mainList.addAll(list)
+        notifyDataSetChanged()
+    }
+}
